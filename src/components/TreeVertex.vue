9d553d5cc721e5bff2c40baaad6678be5e399f11
danya090699 <danya090699@yandex.ru>
2019-07-03 18:11:55 +0400

    tree changed

--- src/components/TreeVertex.vue
+++ src/components/TreeVertex.vue
@@ -1,10 +1,23 @@
 <template>
     <div>
         <div style="display: flex">
-            {{root.label}}
+            <div style="width: 30px; flex-shrink: 0; display: flex; flex-direction: column; align-items: flex-start">
+                <v-icon size="18" color="blue" style="border: 1px solid blue; border-radius: 10px;">add</v-icon>
+                <div style="flex-grow: 1; padding-left: 10px">
+                    <div style="height: 100%; border-left: 1px solid blue"/>
+                </div>
+            <div></div>
+            </div>
+            <div class="title">{{root.label}}</div>
         </div>
         <div v-if="root.children" style="padding-left: 10px">
-            <TreeVertex v-for="child in root.children" :root="child"/>
+            <div v-for="(child, i) in root.children" style="display: flex">
+                <div style="width: 10px; flex-shrink: 0; display: flex; flex-direction: column">
+                    <div style="height: 10px; flex-shrink: 0; border-left: 1px solid blue; border-bottom: 1px solid blue"></div>
+                    <div v-if="i+1<root.children.length" style="flex-grow: 1; border-left: 1px solid blue"></div>
+                </div>
+                <TreeVertex :root="child"/>
+            </div>
         </div>
     </div>
 </template>
